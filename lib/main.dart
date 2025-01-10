353ad0690a9bbca46f3c85d62f9e8cae0b4226d2
Sarang <sarangsalih@gmail.com>
2025-01-10 22:25:38 +0530

    Just Started

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:event_vault/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: ScreenHome(),
     );
   }
 }
