cb49a203ab94d80a880a5f48f180f54a2753ad6f
Sarang <sarangsalih@gmail.com>
2025-01-11 12:27:32 +0530

    Event Manager Started

---
+++ lib/screens/event_manager/event_manager/event_manager.dart
@@ -0,0 +1,98 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ScreenEventManager extends StatefulWidget {
+  const ScreenEventManager({super.key});
+
+  @override
+  State<ScreenEventManager> createState() => _ScreenEventManagerState();
+}
+
+class _ScreenEventManagerState extends State<ScreenEventManager> {
+  List<String> dropDownList = ['Party', 'Catering', 'Decoration'];
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(25, 26, 37, 1),
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Center(
+            child: Text(
+              "Event Manager",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          ),
+        ),
+        backgroundColor: Color.fromRGBO(25, 26, 37, 1),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Search events by name, date or location',
+                hintStyle: TextStyle(color: Color.fromRGBO(124, 125, 144, 1)),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Color.fromRGBO(124, 125, 144, 1),
+                  size: 30,
+                ),
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                      color: Color.fromRGBO(32, 34, 54, 1), width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(color: Colors.transparent),
+                ),
+                fillColor: Color.fromRGBO(32, 34, 54, 1),
+                filled: true,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Filters',
+                style: GoogleFonts.roboto(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 23,
+                ),
+              ),
+            ),
+            DropdownButtonFormField(
+                onChanged: (value) {},
+                items: dropDownList.map(
+                  (e) {
+                    return DropdownMenuItem(
+                      value: e,
+                      child: Text(
+                        e,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
+                  },
+                ).toList(),
+                decoration: InputDecoration(
+                  hintText: 'Choose an option',
+                  // icon: Icon(Icons.arrow_drop_down),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.transparent, width: 2),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,13 +1,82 @@
+import 'package:event_vault/screens/event_manager/event_manager/event_manager.dart';
 import 'package:flutter/material.dart';
 
-class ScreenHome extends StatelessWidget {
-  const ScreenHome({super.key});
+class ScreenMain extends StatefulWidget {
+  const ScreenMain({super.key});
+
+  @override
+  State<ScreenMain> createState() => _ScreenHomeState();
+}
+
+class _ScreenHomeState extends State<ScreenMain> {
+  List<Widget> screenList = [
+    HomeScreen(),
+    ScreenEventManager(),
+    Center(
+        child: Text('New Event',
+            style: TextStyle(color: Colors.white, fontSize: 24))),
+    Center(
+        child: Text('Task Manager',
+            style: TextStyle(color: Colors.white, fontSize: 24))),
+    Center(
+        child: Text('Settings',
+            style: TextStyle(color: Colors.white, fontSize: 24))),
+  ];
+  int myIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(child: Column(children: [],)),
+      backgroundColor: Color.fromRGBO(25, 26, 37, 1),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color.fromRGBO(32, 34, 54, 1),
+        selectedItemColor: Color.fromRGBO(59, 121, 255, 1),
+        unselectedItemColor: Color.fromRGBO(152, 152, 159, 1),
+        iconSize: 30,
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
+        currentIndex: myIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.event), label: 'Events'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline), label: 'New'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.task_outlined), label: 'Task'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+      body: SafeArea(
+        child: screenList[myIndex],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(25, 26, 37, 1),
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Center(
+            child: Text(
+              "Dashboard",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          ),
+        ),
+        backgroundColor: Color.fromRGBO(25, 26, 37, 1),
+      ),
     );
   }
 }
